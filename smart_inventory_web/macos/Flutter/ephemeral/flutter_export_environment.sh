39d6dd9d88ad3c8940c52990f59f21c2cffec341
Thedas Silva <thedassilva64@gmail.com>
2026-08-01 16:53:45 +0530

    Update flutter_export_environment.sh

--- smart_inventory_web/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ smart_inventory_web/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -2,6 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/homebrew/share/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/thedas/Desktop/Projects/R26-IT-060/smart_inventory_web"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/thedas/Desktop/Projects/R26-IT-060/smart_inventory_web/macos/Flutter/ephemeral/Packages/.packages/FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
